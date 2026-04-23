765513596d26d5465632eb06c36ff4f3527960d4
v-amna <amnamampad@gmail.com>
2026-04-23 15:09:32 +0200

    Add Procfile, update settings.py for PostgreSQL, and modify requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -97,10 +97,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #}
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB_NAME"),
+        "USER": os.environ.get("POSTGRES_DB_USERNAME"),
+        "PASSWORD": os.environ.get("POSTGRES_DB_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_DB_HOST"),
+        "PORT": "5432",
     }
 }
 
